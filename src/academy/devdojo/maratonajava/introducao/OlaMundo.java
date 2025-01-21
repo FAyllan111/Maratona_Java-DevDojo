7a5296041b87083604fd94c0f654843dbd655b9d
Ayllan Alves <franciscoayllan@gmail.com>
2025-01-21 16:50:06 -0300

    chore: atualização commit inicial

--- src/academy/devdojo/maratonajava/introducao/OlaMundo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaMundo.java
@@ -1,3 +1,6 @@
+package academy.devdojo.maratonajava.introducao;
+// estrutura do pacote => url do site ao contrario,nome do que é o pacote e descrição do pacote e cada .(ponto) determina que uma pasta vai ser criada (convencao)
+
 public class OlaMundo {
     public static void main(String[] args){
         System.out.println("ellow uordi!!");
