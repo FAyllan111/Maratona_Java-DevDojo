798d960a57bbe3fcef89c333d8c48d0e92d0af9d
Ayllan Alves <franciscoayllan@gmail.com>
2025-01-21 17:28:09 -0300

    chore: tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula01OlaMundo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaMundo.java
@@ -1,7 +1,7 @@
 package academy.devdojo.maratonajava.introducao;
 // estrutura do pacote => url do site ao contrario,nome do que é o pacote e descrição do pacote e cada .(ponto) determina que uma pasta vai ser criada (convencao)
 
-public class OlaMundo {
+public class Aula01OlaMundo {
     public static void main(String[] args){
         System.out.println("ellow uordi!!");
     }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+//atalho criar tudo automatico => psvm
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+          //tipos primitivos => guardam em memoria valores simples
+        // int,double,float,char,byte,short,long,boolean
+        //variavel convencao primeira minuscula e as demais maiusculs
+        // os tamanhos de memoria se mantem os mesmos do estudado no c#
+        int idade = 4;
+        double dupla = 5695425 ;
+        float flutuante = 2586.69f;
+        byte bit = 120;
+        short curto = 258;
+        long longo = 2384745;
+        boolean boleano = true;
+        char caractere = 'm';
+
+        System.out.println(idade);
+        System.out.println(dupla);
+        System.out.println(flutuante);
+        System.out.println(bit);
+        System.out.println(curto);
+        System.out.println(longo);
+        System.out.println(boleano);
+        System.out.println(caractere);
+
+    }
+
+}
