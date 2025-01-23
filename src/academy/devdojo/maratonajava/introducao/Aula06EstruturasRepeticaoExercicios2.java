18bca62178f15ad7aa8cfcb03e956ea6446bcc49
Ayllan Alves <franciscoayllan@gmail.com>
2025-01-23 17:12:59 -0300

    chore : Finalização estuturas repetiçao

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasRepeticaoExercicios2.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasRepeticaoExercicios2.java
@@ -1,4 +1,16 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasRepeticaoExercicios2 {
+    public static void main(String[] args) {
+        //dado um valor de um carro, descubra em quantas vezes ele pode ser parcelado
+        //condição = valor parcela >= 1000
+        double valorCarro = 30000;
+        for (int parcela = 1; parcela <= valorCarro; parcela++) {
+            double valorParcela = valorCarro/parcela;
+            if (valorParcela<1000){
+                break;
+            }
+            System.out.println("Parcela "+ parcela+ " R$: " +valorParcela);
+        }
+    }
 }
