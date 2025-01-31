90ea074d58f8aefa5d2c9eab7b9b98e0a6a00f21
Ayllan Alves <franciscoayllan@gmail.com>
2025-01-31 15:25:18 -0300

    Chore:sobreescrita do toString

---
+++ src/academy/devdojo/maratonajava/javacore/Isobreescrita/test/AnimeTeste01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Isobreescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Isobreescrita.domain.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Initial D");
+
+        System.out.println(anime);
+
+    }
+}
